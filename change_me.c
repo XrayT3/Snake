f94d325ce1bde0ce58ce04d36485eaf0fdbc29b2
XrayT3 <sha0006@yandex.ru>
2020-06-08 23:24:42 +0200

    Update change_me.c

--- change_me.c
+++ change_me.c
@@ -96,7 +96,7 @@ int main(int argc, char *argv[]) {
   for (i=0; i<30; i++) {
      *(volatile uint32_t*)(mem_base + SPILED_REG_LED_LINE_o) = val_line;
      val_line<<=1;
-     printf("LED val 0x%x\n", val_line);
+     //printf("LED val 0x%x\n", val_line);
      clock_nanosleep(CLOCK_MONOTONIC, 0, &loop_delay, NULL);
   }
   
