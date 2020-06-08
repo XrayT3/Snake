31c9786e594ff328ded73b60d3cfad06083453c4
XrayT3 <sha0006@yandex.ru>
2020-06-09 00:20:13 +0200

    Update change_me.c

--- change_me.c
+++ change_me.c
@@ -14,18 +14,18 @@
 #include "font_prop14x16.c"
 
 unsigned short *fb;
+int scale = 5;
 
 void draw_pixel(int x, int y, unsigned short color) {
   if (x>=0 && x<480 && y>=0 && y<320) {
-    printf("+");
     fb[x+480*y] = color;
   }
 }
 
-void draw_pixel8(int x, int y, unsigned short color) {
+void draw_pixel_scale(int x, int y, unsigned short color) {
   int i, j;
-  for (i = 0; i < 8; i++){
-    for (j = 0; j < 8; j++){
+  for (i = 0; i < scale; i++){
+    for (j = 0; j < scale; j++){
       draw_pixel(x+i, y+i, color);
     }
   }
@@ -61,8 +61,7 @@ void draw_char(int x, int y, font_descriptor_t* fdes, char ch, unsigned char col
       font_bits_t val = *ptr;
       for (j = 0; j < w; j++){
         if ((val&0x8000) != 0) {
-          printf("-");
-          draw_pixel8(x+8*j, y+8*i, color);
+          draw_pixel8(x+scale*j, y+scale*i, color);
         }
         val<<=1;
       }
@@ -75,14 +74,10 @@ int main(int argc, char *argv[]) {
   unsigned char *mem_base;
   unsigned char *parlcd_mem_base;
   uint32_t val_line=5;
-  int i,j,k;
+  int i,j;
   int ptr;
   unsigned int c;
   fb  = (unsigned short *)malloc(320*480*2);
-  
-  // printf("Hello world\n");
-
-  // sleep(1);
 
   /*
    * Setup memory mapping which provides access to the peripheral
@@ -118,29 +113,6 @@ int main(int argc, char *argv[]) {
       parlcd_write_data(parlcd_mem_base, fb[ptr++]);
     }
   }
-
-  loop_delay.tv_sec = 0;
-  //loop_delay.tv_nsec = 150 * 1000 * 1000;
-  loop_delay.tv_nsec = 150 * 100 * 100;
-  // for (k=0; k<60; k++) {
-    
-  //   for (ptr = 0; ptr < 320*480 ; ptr++) {
-  //       fb[ptr]=0u;
-  //   }
-  //   // pixel (x,y) -> fb[x+y*480]
-  //   // for (i=0; i<200; i++) {
-  //   //   for (j=0; j<20; j++) {
-  //   //     fb[(i+k)+j+(i+k)*480]=0x1f<<5;
-  //   //   }
-  //   // }
-    
-  //   parlcd_write_cmd(parlcd_mem_base, 0x2c);
-  //   for (ptr = 0; ptr < 480*320 ; ptr++) {
-  //       parlcd_write_data(parlcd_mem_base, fb[ptr]);
-  //   }
-
-  //   clock_nanosleep(CLOCK_MONOTONIC, 0, &loop_delay, NULL);
-  // }
   
   int x = 10;
   char str[]="Goodbye world";
@@ -151,7 +123,7 @@ int main(int argc, char *argv[]) {
   }
   for (i=0; i<13; i++) {
     draw_char(x, 10, fdes, *ch, 0x1f<<11);
-    x+=8*char_width(fdes, *ch)+2;
+    x+=scale*char_width(fdes, *ch)+2;
     ch++;
   }
 
