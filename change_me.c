58dd13a790788e43559fbb234a9e8c0e005149af
XrayT3 <sha0006@yandex.ru>
2020-06-09 15:45:29 +0200

    Update change_me.c

--- change_me.c
+++ change_me.c
@@ -14,12 +14,12 @@
 #include "font_prop14x16.c"
 
 unsigned short *fb;
-int scale = 5;
+int scale = 4;
 
 void draw_pixel(int x, int y, unsigned short color) {
   if (x>=0 && x<480 && y>=0 && y<320) {
     fb[x+480*y] = color;
-    fb[x+480*y] = 0xF81F;
+    //fb[x+480*y] = 0xF81F;
   }
 }
 
@@ -91,13 +91,15 @@ int main(int argc, char *argv[]) {
     exit(1);
 
   struct timespec loop_delay = {.tv_sec = 0, .tv_nsec = 20 * 1000 * 1000};
+
   while (1)
   {
+    val_line = 5;
     for (i=0; i<30; i++) {
-     *(volatile uint32_t*)(mem_base + SPILED_REG_LED_LINE_o) = val_line;
-     val_line<<=1;
-     printf("LED val 0x%x\n", val_line);
-     clock_nanosleep(CLOCK_MONOTONIC, 0, &loop_delay, NULL);
+      *(volatile uint32_t*)(mem_base + SPILED_REG_LED_LINE_o) = val_line;
+      val_line<<=1;
+      printf("LED val 0x%x\n", val_line);
+      clock_nanosleep(CLOCK_MONOTONIC, 0, &loop_delay, NULL);
     }
   }
   
