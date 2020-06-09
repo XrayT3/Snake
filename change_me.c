0db003b591fd44711dd31fc80f643dee31e489a3
XrayT3 <sha0006@yandex.ru>
2020-06-09 15:18:41 +0200

    Update change_me.c

--- change_me.c
+++ change_me.c
@@ -19,7 +19,7 @@ int scale = 4;
 void draw_pixel(int x, int y, unsigned short color) {
   if (x>=0 && x<480 && y>=0 && y<320) {
     fb[x+480*y] = color;
-    fb[x+480*y] = 0xF81C;
+    //fb[x+480*y] = 0xF81F;
   }
 }
 
@@ -119,11 +119,12 @@ int main(int argc, char *argv[]) {
   char str[]="Goodbye world";
   char *ch=str;
   font_descriptor_t* fdes = &font_winFreeSystem14x16;
+  int colour = 0;
   for (ptr = 0; ptr < 320*480 ; ptr++) {
     fb[ptr]=0u;
   }
   for (i=0; i<13; i++) {
-    draw_char(x, 10, fdes, *ch, 0x1f<<11);
+    draw_char(x, 10, fdes, *ch, 0xF81F);
     x+=scale*char_width(fdes, *ch)+2;
     ch++;
   }
