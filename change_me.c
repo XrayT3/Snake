a8ecc3776ff87adba7a66235c93c2409d3a795d2
XrayT3 <sha0006@yandex.ru>
2020-06-09 21:56:25 +0200

    Update change_me.c

--- change_me.c
+++ change_me.c
@@ -121,6 +121,7 @@ int main(int argc, char *argv[]) {
   font_descriptor_t* fdes = &font_winFreeSystem14x16;
   for (ptr = 0; ptr < 320*480 ; ptr++) {
     fb[ptr]=0u;
+    fb[ptr]=0x1f<<11;
   }
   for (i=0; i<15; i++) {
     draw_char(x, 10, fdes, *ch, 0x19);
