960eab5ec70709706c64f6a4c9a08a73c450e6ab
XrayT3 <sha0006@yandex.ru>
2020-06-09 17:25:20 +0200

    Update change_me.c

--- change_me.c
+++ change_me.c
@@ -91,6 +91,7 @@ int main(int argc, char *argv[]) {
     exit(1);
 
   rgb_knobs_value = *(volatile uint32_t*)(mem_base + SPILED_REG_KNOBS_8BIT_o);
+  rgb_knobs_value = 255;
 
   *(volatile uint32_t*)(mem_base + SPILED_REG_LED_RGB1_o) = rgb_knobs_value;
   *(volatile uint32_t*)(mem_base + SPILED_REG_LED_RGB2_o) = rgb_knobs_value;
