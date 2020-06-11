9190084663f52a04af36ea6fec21ad1d1111e6b9
XrayT3 <sha0006@yandex.ru>
2020-06-11 02:00:05 +0200

    Add files via upload

--- snake.c
+++ snake.c
@@ -17,7 +17,7 @@ unsigned short *fb;
 font_descriptor_t* fdes = &font_winFreeSystem14x16;
 unsigned char *parlcd_mem_base;
 int size_cell = 20;
-int size_score = 3;
+int size_score = 5;
 
 snake_t initSnake(int displayWidth, int displayHeight, int initSnakeX, int initSnakeY) {
 
