276762f4c53ea4c5456f0dca0cd2cadeb5f38f75
XrayT3 <sha0006@yandex.ru>
2020-06-11 13:55:01 +0200

    Add files via upload

--- snake.c
+++ snake.c
@@ -10,7 +10,7 @@
 #include "mzapo_parlcd.h"
 #include "mzapo_phys.h"
 #include "mzapo_regs.h"
-#include "font_types.h"
+// #include "font_types.h"
 // #include "font_prop14x16.c"
 
 unsigned short *fb;
