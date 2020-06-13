1fcc68e2f628d7007fa252a9afcebdfc4987272f
XrayT3 <sha0006@yandex.ru>
2020-06-13 23:11:22 +0200

    Add files via upload

--- snake_test.c
+++ snake_test.c
@@ -125,10 +125,10 @@ int main() {
                 printf("AI\n\r");
             }
         }
-        parlcd_write_cmd(parlcd_mem_base, 0x2c);
-        for (ptr = 0; ptr < 480*320 ; ptr++) {
-            parlcd_write_data(parlcd_mem_base, fb[ptr]);
-        }
+        // parlcd_write_cmd(parlcd_mem_base, 0x2c);
+        // for (ptr = 0; ptr < 480*320 ; ptr++) {
+        //     parlcd_write_data(parlcd_mem_base, fb[ptr]);
+        // }
     }
     sleep(1);
     draw_EndGame(fb, snake->score, retry, quit);
@@ -169,10 +169,10 @@ int main() {
     Menu:
     draw_Menu(fb, standard, demo);
     // draw LCD
-    parlcd_write_cmd(parlcd_mem_base, 0x2c);
-    for (ptr = 0; ptr < 480*320 ; ptr++) {
-        parlcd_write_data(parlcd_mem_base, fb[ptr]);
-    }
+    // parlcd_write_cmd(parlcd_mem_base, 0x2c);
+    // for (ptr = 0; ptr < 480*320 ; ptr++) {
+    //     parlcd_write_data(parlcd_mem_base, fb[ptr]);
+    // }
     //draw LED
     rgb_knobs_value = *(volatile uint32_t*)(mem_base + SPILED_REG_KNOBS_8BIT_o);
     rgb_knobs_value = 16711935; //purple
@@ -199,10 +199,10 @@ int main() {
             }
         }
         // draw LCD
-        parlcd_write_cmd(parlcd_mem_base, 0x2c);
-        for (ptr = 0; ptr < 480*320 ; ptr++) {
-            parlcd_write_data(parlcd_mem_base, fb[ptr]);
-        }
+        // parlcd_write_cmd(parlcd_mem_base, 0x2c);
+        // for (ptr = 0; ptr < 480*320 ; ptr++) {
+        //     parlcd_write_data(parlcd_mem_base, fb[ptr]);
+        // }
     }
     goto start_game;
 
