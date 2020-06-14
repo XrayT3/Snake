a533593f6180912dafa22809b70f2e4bcdfb7f81
XrayT3 <sha0006@yandex.ru>
2020-06-14 11:46:10 +0200

    Add files via upload

--- snake.h
+++ snake.h
@@ -42,17 +42,17 @@ typedef struct {
     int gameOver;
 } desk_t;
 
-void draw_pixel(int x, int y, int color);
+// void draw_pixel(int x, int y, int color);
 
-void draw_EndGame(unsigned short *fb1, int score, int retry, int quit);
+// void draw_EndGame(unsigned short *fb1, int score, int retry, int quit);
 
-void draw_Menu(unsigned short *fb1, int standard, int demo, int exit);
+// void draw_Menu(unsigned short *fb1, int standard, int demo, int exit);
 
-void draw_speed_ctrl(unsigned short *fb1, int slow, int medium, int fast);
+// void draw_speed_ctrl(unsigned short *fb1, int slow, int medium, int fast);
 
-void drawDesk(desk_t *desk, snake_t *snake, food_t *food, int sec, unsigned short *fb1);
+// void drawDesk(desk_t *desk, snake_t *snake, food_t *food, int sec, unsigned short *fb1);
 
-void drawDesk_2_snakes(desk_t *desk, snake_t *snake, snake_t *snake2, food_t *food, int sec, unsigned short *fb1);
+// void drawDesk_2_snakes(desk_t *desk, snake_t *snake, snake_t *snake2, food_t *food, int sec, unsigned short *fb1);
 
 //Initialize snake that will be controlled manually
 snake_t *initSnake(int displayWidth, int displayHeight, int initSnakeX, int initSnakeY, char turnLeft, char turnRight);
