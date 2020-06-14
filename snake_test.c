bd17003ad133406685bf5c28a491ee6fe7a9e7f1
XrayT3 <sha0006@yandex.ru>
2020-06-14 02:35:36 +0200

    Add files via upload

--- snake_test.c
+++ snake_test.c
@@ -20,6 +20,7 @@ int scale = 5;
 int speed = 100;
 int standard = 0;
 int demo = 1;
+int exit = 0;
 int retry = 1;
 int quit = 0;
 int slow = 1;
@@ -60,7 +61,7 @@ int main() {
 
     val_line = 15;
     *(volatile uint32_t*)(mem_base + SPILED_REG_LED_LINE_o) = val_line;
-    struct timespec loop_delay = {.tv_sec = 0, .tv_nsec = 50 * 1000 * 1000};
+    struct timespec loop_delay = {.tv_sec = 0, .tv_nsec = 20 * 1000 * 1000};
     for (i=0; i<30; i++) {
         *(volatile uint32_t*)(mem_base + SPILED_REG_LED_LINE_o) = val_line;
         val_line<<=1;
@@ -193,14 +194,19 @@ int main() {
         int r = read(0, &ch, 1);
         if (r==1)
         {   
+            a = standard;
+            b = demo;
+            c = exit;
             if (ch == 'w') {
-                standard = 1 - standard;
-                demo = 1 - demo;
+                standard = c;
+                demo = a;
+                exit = b;
                 draw_Menu(fb, standard, demo);
             }
             else if (ch == 's') {
-                standard = 1 - standard;
-                demo = 1 - demo;
+                standard = b;
+                demo = c;
+                exit = a;
                 draw_Menu(fb, standard, demo);
             }
             else if (ch == ' ') {
@@ -208,7 +214,10 @@ int main() {
             }
         }
     }
-    goto start_game;
+    
+    if (1-exit){
+        goto start_game;
+    }
 
     // exit:
     rgb_knobs_value = *(volatile uint32_t*)(mem_base + SPILED_REG_KNOBS_8BIT_o);
