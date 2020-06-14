30e8272365579f43586d9b024cf2729a9eca82e7
XrayT3 <sha0006@yandex.ru>
2020-06-14 11:58:47 +0200

    Add files via upload

--- menu.h
+++ menu.h
@@ -1,41 +1,6 @@
 #include "font_types.h"
 // #include "snake.h"
-
-//Snake food data type
-typedef struct {
-    
-    int coord[2];
-} food_t;
-
-//Snake section data type
-typedef struct {
-
-    int coords[2];
-    int validity;
-} section_t;
-
-//Snake data type
-typedef struct {
-    int length;
-    int max_length;
-    int score;
-    int direction;
-    char turnLeft;
-    char turnRight;
-    section_t *snake_skeleton;
-} snake_t;
-
-//Game field data type
-typedef struct {
-
-    int width;
-    int height;
-    int startX;
-    int startY;
-    int endX;
-    int endY;
-    int gameOver;
-} desk_t;
+#include "snake_types.h"
 
 
 void draw_pixel(int x, int y, int color);
--- snake.h
+++ snake.h
@@ -5,42 +5,43 @@
 #define INIT_LEN 2
 
 #include "font_types.h"
+#include "snake_types.h"
 
-//Snake food data type
-typedef struct {
+// //Snake food data type
+// typedef struct {
     
-    int coord[2];
-} food_t;
+//     int coord[2];
+// } food_t;
 
-//Snake section data type
-typedef struct {
+// //Snake section data type
+// typedef struct {
 
-    int coords[2];
-    int validity;
-} section_t;
+//     int coords[2];
+//     int validity;
+// } section_t;
 
-//Snake data type
-typedef struct {
-    int length;
-    int max_length;
-    int score;
-    int direction;
-    char turnLeft;
-    char turnRight;
-    section_t *snake_skeleton;
-} snake_t;
+// //Snake data type
+// typedef struct {
+//     int length;
+//     int max_length;
+//     int score;
+//     int direction;
+//     char turnLeft;
+//     char turnRight;
+//     section_t *snake_skeleton;
+// } snake_t;
 
-//Game field data type
-typedef struct {
+// //Game field data type
+// typedef struct {
 
-    int width;
-    int height;
-    int startX;
-    int startY;
-    int endX;
-    int endY;
-    int gameOver;
-} desk_t;
+//     int width;
+//     int height;
+//     int startX;
+//     int startY;
+//     int endX;
+//     int endY;
+//     int gameOver;
+// } desk_t;
 
 // void draw_pixel(int x, int y, int color);
 
